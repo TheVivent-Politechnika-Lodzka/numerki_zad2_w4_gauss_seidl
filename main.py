393163e0778e925d421c136b87f88180fd7dd673
Adam Vivent Kapuściński <a.kapuscinski@thevivent.com>
2021-03-22 16:30:09 +0100

    wczytywanie macierzy z pliku

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+import numpy as np
+
+
+matrix = []
+with open("matrix.txt") as file_in:
+    for line in file_in:
+        line = line.replace('\n', '')
+        matrix.append(list(map(int, line.split())))
+matrix = np.array(matrix)
+
+print(matrix)
